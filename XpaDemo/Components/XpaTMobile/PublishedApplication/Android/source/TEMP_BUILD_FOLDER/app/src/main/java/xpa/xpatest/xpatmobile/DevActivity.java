6bb45739b53f3de13a603b7e19fee89fda549c18
Andreas Sedlmeier <sedlmeier@hotmail.com>
2018-06-15 18:18:32 +0200

    OWCLN001_xpatest_uris

--- XpaDemo/Components/XpaTMobile/PublishedApplication/Android/source/TEMP_BUILD_FOLDER/app/src/main/java/xpa/xpatest/xpatmobile/DevActivity.java
+++
@@ -1,7 +0,0 @@
-package xpa.xpatest.xpatmobile;
-
-import com.magicsoftware.core.startact;
-
-public class DevActivity extends startact {
-	
-}
--- XpaDemo/Components/XpaTMobile/PublishedApplication/Android/source/app/src/main/java/com/magicsoftware/magicdev/DevActivity.java
+++
@@ -1,7 +0,0 @@
-package com.magicsoftware.magicdev;
-
-import com.magicsoftware.core.startact;
-
-public class DevActivity extends startact {
-	
-}
