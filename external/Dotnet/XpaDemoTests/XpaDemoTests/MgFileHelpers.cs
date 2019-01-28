a5ab62948bc9b83c94873934ddd2f71ec82ae4b1
Andreas Sedlmeier <a.sedlmeier@cis-solutions.eu>
2019-01-28 12:06:08 +0100

    added XpaTMail

---
+++ external/Dotnet/XpaDemoTests/XpaDemoTests/MgFileHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XpaDemoTests
+{
+    public class MgFileHelpers
+    {
+        public FileSystemInfo[] FileListGet(String directory, String orderby) {
+            List<FileSystemInfo> filesOrdered, files = new DirectoryInfo(directory).GetFileSystemInfos().ToList();
+
+            switch (orderby) {
+                case "CreationTime":
+                    filesOrdered = files.OrderBy(f => f.CreationTime).ToList();
+                    break;
+                case "Extension":
+                    filesOrdered = files.OrderBy(f => f.Extension).ToList();
+                    break;
+                case "FullName":
+                    filesOrdered = files.OrderBy(f => f.FullName).ToList();
+                    break;
+                case "Name":
+                default:
+                    filesOrdered = files.OrderBy(f => f.Name).ToList();
+                    break;
+            }
+            if (orderby == "CreationTime ")
+                filesOrdered = files.OrderBy(f => f.CreationTime).ToList();
+            return filesOrdered.ToArray();
+        }
+    }
+}
